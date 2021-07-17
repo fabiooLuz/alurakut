617540107a135fef5a01d64f8e65cf06fd72f9fb
fabiooluz <fabioanderson_sb@hotmail.com>
2021-07-17 01:18:50 -0300

    Aula 5

--- pages/api/comunidades.js
+++ pages/api/comunidades.js
@@ -2,16 +2,16 @@ import { SiteClient } from 'datocms-client';
 
 export default async function recebedorDeRequests(request, response) {
     if(request.method === 'POST') {
-        const TOKEN = '3a9782340b8b4df0d359bb792b880b';
+        const TOKEN = 'b1edaab5abf7c111dfa41db54a751b';
         const client = new SiteClient(TOKEN);
         
         // Validar os dados, antes de sair cadastrando
         const registroCriado = await client.items.create({
-            itemType: "836335", // ID do Model de "Communities" criado pelo Dato
+            itemType: "972890", // ID do Model de "Communities" criado pelo Dato
             ...request.body,
-            // title: "Comunidade de Teste",
-            // imageUrl: "https://github.com/omariosouto.png",
-            // creatorSlug: "omariosouto"
+             //title: "Comunidade de Teste",
+             //imageUrl: "https://github.com/fabiooluz.png",
+             //creatorSlug: "fabiooluz"
         })
     
         console.log(registroCriado);
